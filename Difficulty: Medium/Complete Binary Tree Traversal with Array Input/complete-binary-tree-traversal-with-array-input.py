25235b2fb55d65a4f6f3e089d067ea63f0f811ae
Uday Sen <vickysain817@gmail.com>
2026-07-26 06:02:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Complete Binary Tree Traversal with Array Input/complete-binary-tree-traversal-with-array-input.py
+++ Difficulty: Medium/Complete Binary Tree Traversal with Array Input/complete-binary-tree-traversal-with-array-input.py
@@ -6,14 +6,14 @@ class Solution:
         n = len(arr)
         
         while i < n:
-           
+            # Slice elements for the current level
             current_level = arr[i : i + level_size]
             
-            
+            # Sort the current level in ascending order
             current_level.sort()
             ans.append(current_level)
             
-           
+            # Move index forward and double the level capacity for next level
             i += level_size
             level_size *= 2
             
